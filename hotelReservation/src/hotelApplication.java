3ff8204759ee8d58bd284ab24e3ae9e2d41013cc
abakopolus <108642798+abakopolus@users.noreply.github.com>
2024-07-05 13:10:40 -0400

    Delete hotelReservation directory

--- hotelReservation/src/hotelApplication.java
+++
@@ -1,8 +0,0 @@
-/**
- * Driver file for Hotel Room Booking Application
- */
-public class hotelApplication {
-    public static void main(String[] args) {
-        mainMenu.mainMenuNavigation();
-    }
-}
